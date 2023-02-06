f165ebca8487e77eb5801e0483f72c3ae2c8ce5e
Robert Gripshover <robertgrips@gmail.com>
2023-02-06 17:30:17 -0500

    I made a new folder called components and a new file inside that called FreindsList that I just put a simple message in to make sure it was displaying inside my App.js which it is being displayed in

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import FreindList from './components/FreindsList'; //<< the FreindList component I am making from scratch
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <h2>Client Auth Project</h2>
+      <FreindList />
     </div>
   );
 }
---
+++ src/components/FreindsList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+
+const FreindList = () => {
+
+    return (
+        <div>This seems to be working!</div>
+    )
+}
+
+export default FreindList //<< this is being import and displayed inside App.js
