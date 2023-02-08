d49f16ac167bf8bff10b2918b3dd5525adfe0632
Robert Gripshover <robertgrips@gmail.com>
2023-02-08 15:40:33 -0500

    working on making the new friend form submit

--- src/components/AddFriends.js
+++ src/components/AddFriends.js
@@ -1,26 +1,63 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useHistory} from 'react-router-dom'
+import axios from 'axios'
+
 
 const AddFriend = () => {
+
+  const {push} = useHistory()
+
+  const [formData, setFormData] = useState({
+    name: '',
+    age: '', 
+    email: ''
+  })
+
+ 
+  const handleChange = (e) => {
+    e.preventDefault();
+    setFormData({
+        ...formData,
+        [e.target.name]: e.target.value
+    })        
+}
+
+const submitFriendForm = (e) => {
+  e.preventDefault()
+  console.log('Clicked :)')
+  axios.post(`http://localhost:9000/api/friends`, formData)
+      .then(resp => {
+          console.log(resp)
+          localStorage.setItem('token', resp.data.token)
+          push('/friends')
+      })
+      .catch(err => {
+          console.log(err)
+      })
+}
+
+console.log(formData)
+
     return(
       <div>
         <h2>Add Friend</h2>
-        <form>
+        <form onSubmit={submitFriendForm}>
             <div>
-                <label htmlForm='username'>Username:</label>
-                <input id='usersame'/>
+                <label htmlform='name'>Name:</label>
+                <input onChange={handleChange} name='name' id='name'/>
             </div>
 
             <div>
-                <label htmlForm='age'>Age:</label>
-                <input id='age'/>
+                <label htmlform='age'>Age:</label>
+                <input onChange={handleChange} name='age' id='age'/>
             </div>
 
             <div>
-                <label htmlForm='email'>Email:</label>
-                <input id='email'/>
+                <label htmlform='email'>Email:</label>
+                <input onChange={handleChange} name='email' id='email'/>
             </div> 
+            <button>Click to Add the Friend</button>
         </form>
-        <button>Click to Add the Friend</button>
       </div>
     )
   }
